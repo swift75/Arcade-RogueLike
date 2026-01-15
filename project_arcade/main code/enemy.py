44757b6b5affeffd6a602f40e7efe752ada9ecf4
Ruslan <taskaraevruslan5@gmail.com>
2026-01-15 19:08:30 +0300

    4

--- project_arcade/main code/enemy.py
+++ project_arcade/main code/enemy.py
@@ -4,16 +4,54 @@ import random
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+ENEMY_DATA = {
+    "fly": {
+        "color": arcade.color.LIGHT_GRAY,
+        "hp": 20,
+        "attack": 4,
+        "defense": 1,
+    },
+    "goblin": {
+        "color": arcade.color.GREEN,
+        "hp": 45,
+        "attack": 9,
+        "defense": 4,
+    },
+    "skeleton": {
+        "color": arcade.color.BONE,
+        "hp": 40,
+        "attack": 10,
+        "defense": 3,
+    },
+    "mushroom": {
+        "color": arcade.color.RED_ORANGE,
+        "hp": 80,
+        "attack": 15,
+        "defense": 7,
+    },
+}
+
 
 class Enemy(arcade.SpriteSolidColor):
-    def __init__(self):
-        super().__init__(50, 50, arcade.color.RED)
+    def __init__(self, lvl):
+        self.enemy_type = random.choice(list(ENEMY_DATA.keys()))
+        data = ENEMY_DATA[self.enemy_type]
+
+        super().__init__(50, 50, data["color"])
+
         self.center_x = random.randint(150, SCREEN_WIDTH - 150)
         self.center_y = random.randint(200, SCREEN_HEIGHT - 200)
 
+        coef = 1 + lvl / 10
+
+        self.max_hp = int(data["hp"] * coef)
+        self.hp = self.max_hp
+        self.attack = int(data["attack"] * coef)
+        self.defense = int(data["defense"] * coef)
+
     def draw_label(self):
         arcade.draw_text(
-            "ENEMY",
+            self.enemy_type.upper(),
             self.center_x,
             self.center_y + 35,
             arcade.color.WHITE,
